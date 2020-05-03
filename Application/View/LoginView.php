85abdceec27fb9c0b26eafad19cdc680b4fa492e
nils.jung <nils.m.jung@student.fh-kiel.de>
2020-05-03 11:56:29 +0200

    fix login error message

--- Application/View/LoginView.php
+++ Application/View/LoginView.php
@@ -4,6 +4,15 @@ namespace View;
 
 class LoginView extends View {
 
+    public function __construct( $controller, $model ) {
+        parent::__construct($controller, $model);
+        $this->template->hasValidationError = false;
+    }
+
+    public function validationError() {
+        $this->template->hasValidationError = true;
+    }
+
     /**
      * @return mixed
      */
