330678fb8b81ce3a265ba11bb44243a45d41b7cb
nils.jung <nils.m.jung@student.fh-kiel.de>
2020-05-03 11:56:52 +0200

    add active route for navigation

--- Application/View/NavigationView.php
+++ Application/View/NavigationView.php
@@ -2,14 +2,21 @@
 
 namespace View;
 
+use Controller\SessionController;
+
 class NavigationView extends View {
 
     public function render() {
-        if ( $_SESSION && $_SESSION[ "isAuthenticated" ] ) {
+        if ( SessionController::isAuthenticated() ) {
             $this->template->isAuthenticated = true;
-            $this->template->userId = $_SESSION[ 'userId' ];
         } else {
             $this->template->isAuthenticated = false;
+        }
+
+        if (array_key_exists('REDIRECT_URL', $_SERVER)) {
+            $this->template->activeRoute = $_SERVER['REDIRECT_URL'];
+        } else {
+            $this->template->activeRoute = '/';
         }
 
         return $this->template->render("Navigation.inc");
