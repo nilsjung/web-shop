fcceee04e38405253603334210d65409bb1aefea
nils.jung <nils.m.jung@student.fh-kiel.de>
2020-05-03 11:10:40 +0200

    improve debug mode in index.php

--- index.php
+++ index.php
@@ -10,14 +10,15 @@ require_once( 'Public/routes.php' );
 
 $navigation = new \View\NavigationView(null, null);
 
+/** Debug Mode */
 $isDebug = false;
-$body[ "debug" ] = "<h2>Debug</h2><table class='table'><tbody>";
-
+$debug_key = "debug";
+$body[ $debug_key ] = "<h2>Debug</h2><table class='table'><tbody>";
 foreach ( $_SESSION as $key => $value ) {
-    $body[ "debug" ] .= "<tr><td class='mr-3'>" . $key . ':</td><td>' . $value . "</td></tr>";
+    $body[ $debug_key ] .= "<tr><td class='mr-3'>" . $key . ':</td><td>' . $value . "</td></tr>";
 }
-
-$body[ "debug" ] .= "</tbody></table>";
+$body[ $debug_key ] .= "</tbody></table>";
+/** End Debug Mode */
 
 $body[ "navigation" ] = $navigation->render();
 
