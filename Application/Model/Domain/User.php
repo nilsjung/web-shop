e57fbbcc0da25a6e5ab835459e1850fa89735aa0
nils.jung <nils.m.jung@student.fh-kiel.de>
2020-05-12 22:13:11 +0200

    update model structure to handle with build in domain models

---
+++ Application/Model/Domain/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Model\Domain;
+
+class User
+{
+    private string $id;
+    private string $firstName;
+    private string $lastName;
+    private string $emailAddress;
+    private string $password;
+    private ShoppingCart $shoppingCart;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * @param mixed $emailAddress
+     */
+    public function setEmailAddress($emailAddress): void
+    {
+        $this->emailAddress = $emailAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShoppingCart()
+    {
+        return $this->shoppingCart;
+    }
+
+    /**
+     * @param mixed $shoppingCart
+     */
+    public function setShoppingCart($shoppingCart): void
+    {
+        $this->shoppingCart = $shoppingCart;
+    }
+}
